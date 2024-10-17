e12d12eb5fa691673d09991bcab3ad9e7705d9d8
yassminenajjar <yasminenajjarb@gmail.com>
2024-10-17 21:54:55 +0100

    commit

---
+++ src/main/java/iset/bizerte/elearning/contoller/CoursController.java
@@ -0,0 +1,52 @@
+package iset.bizerte.elearning.contoller;
+
+import iset.bizerte.elearning.Dto.CoursDto;
+import iset.bizerte.elearning.Service.CoursService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/Cours")
+@RequiredArgsConstructor
+public class CoursController {
+
+    private final CoursService coursService;
+
+    @GetMapping("/listall")
+    public List<CoursDto> findAll() {
+        return coursService.findAll();
+    }
+
+    @GetMapping("/getbyid/{id}")
+    public CoursDto findById(@PathVariable("id") Long id) {
+        return coursService.findById(id);
+    }
+
+    @PostMapping("/save")
+    public CoursDto save(@RequestBody CoursDto request) {
+        return coursService.save(request);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteById(@PathVariable("id") Long id) {
+        coursService.deleteById(id);
+    }
+
+    @GetMapping("/search/{kye}")
+    public List<CoursDto> findbyobjet(@PathVariable("kye") String kye) {
+        return coursService.findbyobjet(kye);
+    }
+
+    @GetMapping("/finddate")
+    public List<CoursDto> findDate( Date start, Date end) {
+        return coursService.findDate(start, end);
+    }
+
+    @PutMapping("/update")
+    public CoursDto update(@RequestBody CoursDto request) {
+        return coursService.uppdate(request);
+    }
+}
